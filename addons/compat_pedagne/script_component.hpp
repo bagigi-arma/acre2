2aa814d794a333807c2f0d91e0549b74d63dea1d
mrschick <58027418+mrschick@users.noreply.github.com>
2024-11-13 13:51:29 +0100

    Testing of Compat Pedagne/Unsung

---
+++ addons/compat_pedagne/script_component.hpp
@@ -0,0 +1,13 @@
+#define COMPONENT compat_pedagne
+#define COMPONENT_BEAUTIFIED Pedagne Compatibility
+#include "\idi\acre\addons\main\script_mod.hpp"
+
+#ifdef DEBUG_ENABLED_COMPAT_PEDAGNE
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_ENABLED_COMPAT_PEDAGNE
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_COMPAT_PEDAGNE
+#endif
+
+#include "\idi\acre\addons\main\script_macros.hpp"
